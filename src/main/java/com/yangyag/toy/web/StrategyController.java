d2c89e03540c617c58f1adec3aefa43d88b2aed9
yangyag <yangyag@hanmail.net>
2024-04-08 15:42:00 +0900

    Application.java 에서 쓸모없는 Bean 등록 삭제

--- src/main/java/com/yangyag/toy/web/StrategyController.java
+++ src/main/java/com/yangyag/toy/web/StrategyController.java
@@ -1,6 +1,7 @@
 package com.yangyag.toy.web;
 
 import com.yangyag.toy.service.StrategyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,13 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class StrategyController {
 
     private final Map<String, StrategyService> strategyMap;
-
-    public StrategyController(Map<String, StrategyService> strategyMap) {
-        this.strategyMap = strategyMap;
-    }
 
     @GetMapping("/strategy/{strategyName}")
     public ResponseEntity<String> executeStrategy(@PathVariable String strategyName) {
